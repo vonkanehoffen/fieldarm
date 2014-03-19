1e3493d5daae4fb4067f6ac4115bd860d160e656
Kane Clover <kane@assure24.com>
2014-03-19 17:54:32 +0000

    CUAR sidebar in BS grid

--- customer-area/templates/customer-page.template.php
+++ customer-area/templates/customer-page.template.php
@@ -6,20 +6,22 @@
 	else $page_classes[] = "cuar-page-without-sidebar";
 ?>
 <div class="cuar-page <?php echo implode( ' ', $page_classes ); ?>">
+
 	<div class="cuar-page-header"><?php 
 		$this->print_page_header( $args, $shortcode_content ); 
 	?></div>
 	
-	<div class="cuar-page-content"><?php 
-		$this->print_page_content( $args, $shortcode_content ); 
-	?></div>
-	
-	<?php if ( $this->has_page_sidebar() ) : ?>	
-	<div class="cuar-page-sidebar"><?php 
-		$this->print_page_sidebar( $args, $shortcode_content ); 
-	?></div>	
-	<?php endif; ?>
-	
+	<div class="row">
+		<div class="cuar-page-content col-sm-8 col-sm-push-4"><?php 
+			$this->print_page_content( $args, $shortcode_content ); 
+		?></div>
+		
+		<?php if ( $this->has_page_sidebar() ) : ?>	
+		<div class="cuar-page-sidebar col-sm-4 col-sm-pull-8"><?php 
+			$this->print_page_sidebar( $args, $shortcode_content ); 
+		?></div>	
+		<?php endif; ?>
+	</div>
 	<div class="cuar-page-footer"><?php 
 		$this->print_page_footer( $args, $shortcode_content ); 
 	?></div>
