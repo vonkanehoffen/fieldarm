1ecb7380de67a4a468e18b85cdb0e3a4e28fdc3f
Kane Clover <kane@assure24.com>
2014-03-25 20:44:40 +0000

    style CUAR side menu

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5b0c2826951682b6c961532752f9ae51');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e60821a928450e42e59b5e1ce5b12aaa');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
