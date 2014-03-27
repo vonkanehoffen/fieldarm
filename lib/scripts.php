40a96240ff5d610bcaa751c2818ec0da1f80635e
Kane Clover <kane@assure24.com>
2014-03-27 10:36:12 +0000

    green footer with cert logos

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8758223241506f51eb3edd0467640cfb');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8ebe965071788cbbe70ad83710693be8');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/footer.php
+++ templates/footer.php
@@ -1,17 +1,11 @@
 <footer class="content-info container" role="contentinfo">
   <div class="row">
     <div class="col-sm-7">
-      <?php dynamic_sidebar('sidebar-footer'); ?>
-      <p class="small">
-      	All material copyright FieldArm Limited 2014. All rights reserved.<br>
-        Registered in the England and Wales No. 6781048<br>
-        ORETO Certified 348 to 31st January 2019<br>
-        BASIS FACTS NPTC EPPO BCPC
-      </p>
+      <p>Regulated by:</p> <div class="icon-cert-logos"></div>
     </div>
-    <div class="col-sm-5">
-      <!-- <div class="footer-logo icon-fieldarm-sm-grey"></div> -->
-      <a href="http://www.linkedin.com/company/5055113" class="icon-linkedin linkedin"><span>LinkedIn</span></a>
+    <div class="col-sm-5 footer-right">
+      <?php dynamic_sidebar('sidebar-footer'); ?>
+      <p class="linkedin"><span>Find us on</span> <a href="http://www.linkedin.com/company/5055113" class="icon-linkedin"><span>LinkedIn</span></a></p>
     </div>
   </div>
 </footer>
