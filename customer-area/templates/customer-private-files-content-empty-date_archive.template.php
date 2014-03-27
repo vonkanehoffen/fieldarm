b608817e84ea73c45453e5be016bc108b30d0262
Kane Clover <kane@assure24.com>
2014-03-27 17:26:37 +0000

    CUAR no files messages

---
+++ customer-area/templates/customer-private-files-content-empty-date_archive.template.php
@@ -0,0 +1,3 @@
+<?php /** Template version: 1.0.0 */ ?>
+
+<div class="alert alert-warning"><span class="glyphicon glyphicon-info-sign"></span> <?php _e( 'There are no files for that period.', 'cuar' ); ?></div>
---
+++ customer-area/templates/customer-private-files-content-empty.template.php
@@ -0,0 +1,3 @@
+<?php /** Template version: 1.0.0 */ ?>
+
+<div class="alert alert-warning"><span class="glyphicon glyphicon-info-sign"></span> <?php _e( 'You currently have no files.', 'cuar' ); ?></div>
