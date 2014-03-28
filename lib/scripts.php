23a44b68ec8a1f2aed18762110cab08796ca4248
Kane Clover <kane@assure24.com>
2014-03-28 08:12:39 +0000

    reinstate sofia pro font for body copy

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '607c873d46f972ddfc4f1c226a7c9308');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2013ac1db1bf19d5321a4c67a901437c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
