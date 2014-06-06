932548ab0e1354a0695fc2e5b03ecbbe85e1730a
Baron VonKaneHoffen <baron.vonkanehoffen@gmail.com>
2014-06-06 13:36:53 -0400

    stop removing the RSD link (needed for WP mobile app)

--- lib/cleanup.php
+++ lib/cleanup.php
@@ -11,7 +11,7 @@ function roots_head_cleanup() {
   // Originally from http://wpengineer.com/1438/wordpress-header/
   remove_action('wp_head', 'feed_links', 2);
   remove_action('wp_head', 'feed_links_extra', 3);
-  remove_action('wp_head', 'rsd_link');
+  //remove_action('wp_head', 'rsd_link');
   remove_action('wp_head', 'wlwmanifest_link');
   remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
   remove_action('wp_head', 'wp_generator');
