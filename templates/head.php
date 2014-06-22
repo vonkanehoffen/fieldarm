08ab8865aaf2a7f5dcf885226e6b0111d9d5755a
Kane Clover <kane@assure24.com>
2014-06-22 19:32:56 +0100

    load fontawesome

--- templates/head.php
+++ templates/head.php
@@ -5,7 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
+  <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+  
   <?php wp_head(); ?>
 
   <script>
